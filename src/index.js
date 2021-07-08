d6e7c35c0cfef2872dfd4008ae628e48a003ba41
marie-tomoishi <36830861+marie-tomoishi@users.noreply.github.com>
2021-07-08 15:35:16 +0900

    削除とか

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const todoList = document.getElementsByClassName('p-todoList')[0];
 //ステータスタブ分け＆タスクがないときの処理
 const statusFilter = () => {
     const todoItem = document.querySelectorAll('.c-card');
+
+    //タブで表示ON・OFF
     todoItem.forEach((item) => {
         const itemStatus = item.dataset.status;
         const tabStatus = document.getElementsByClassName('is-active')[0]
@@ -26,20 +28,22 @@ const statusFilter = () => {
 
     const count = [...todoItem].filter((x) => {
         return x.style.display === 'block';
-    }).length;
-    const editBtn = document.getElementsByClassName('c-edit')[0];
-    const message = document.getElementsByClassName('p-notask')[0];
+    }).length; //表示されているタスクの数を取得
+    const editBtn = document.getElementsByClassName('c-edit')[0]; //編集ボタン
+    const message = document.getElementsByClassName('p-notask')[0]; //タスクがないときのメッセージ
+
     if (count === 0) {
         editBtn.style.display = 'none';
         addBtn.style.display = 'none';
         message.style.display = 'block';
     } else {
         editBtn.style.display = 'block';
-        addBtn.style.display = 'block';
+        addBtn.style.display = 'inline-flex';
         message.style.display = 'none';
     }
 };
 
+//ロード時にステータス確認（合ってるのか...？）
 statusFilter();
 
 //タブ切り替え
@@ -55,10 +59,12 @@ tabItem.forEach((item) => {
     });
 });
 
-//入力フィールド出現
+//入力フィールドを出現させる
 const addInputField = () => {
     const activeTab = document.getElementsByClassName('is-active')[0];
     const allTab = document.querySelector('[data-status="all"]');
+
+    //Doneの場合はAllに移動する
     if (activeTab.dataset.status === 'done') {
         allTab.click();
     }
@@ -67,7 +73,7 @@ const addInputField = () => {
             <button class="c-card__check"></button>
             <input type="text" class="c-card__input">
         </div>
-        <button class="c-card__edit">
+        <button class="c-card__reEdit">
             <i class="icon-edit-2"></i>
         </button>
         <button class="c-card__remove">
@@ -80,10 +86,10 @@ const addInputField = () => {
     document.getElementsByClassName('c-card__input')[0].focus();
 };
 
-//タスク追加
-const addTask = (event) => {
-    if (event.target.classList[0] === 'c-card__input') {
-        const input = event.target;
+//タスクとして確定
+const addTask = (e) => {
+    if (e.target.classList[0] === 'c-card__input') {
+        const input = e.target;
         const todoValue = input.value;
         const body = `<p class="c-card__body">${todoValue}</p>`;
         const inputField = document.querySelector('.-input');
@@ -97,13 +103,22 @@ const addTask = (event) => {
         input.parentNode.removeChild(input);
         addBtn.removeAttribute('disabled');
         floatAddBtn.removeAttribute('disabled');
-        statusFilter();
+        statusFilter(); //ステータス振り分けし直し
+        releaseEdit(); //編集状態は解除する
     }
 };
 
-//ボタンクリックでタスクを追加、フォーカスアウト・エンターで確定
-addBtn.addEventListener('click', () => addInputField());
-floatAddBtn.addEventListener('click', () => addInputField());
+//追加ボタンクリックで入力フィールド出現
+addBtn.addEventListener('click', () => {
+    releaseEdit();
+    addInputField();
+});
+//追加ボタンクリックで入力フィールド出現
+floatAddBtn.addEventListener('click', () => {
+    releaseEdit();
+    addInputField();
+});
+//入力後エンターでフォーカスアウト
 todoList.addEventListener('keydown', (e) => {
     if (e.keyCode === 13 && e.target.classList[0] === 'c-card__input') {
         const input = todoList.getElementsByClassName('-input')[0];
@@ -111,48 +126,99 @@ todoList.addEventListener('keydown', (e) => {
         inputField.blur();
     }
 });
+//フォーカスアウトでタスク確定
 todoList.addEventListener('focusout', (e) => addTask(e));
 
-//チェック
-todoList.addEventListener('click', (e) => {
-    if (e.target.classList[0] === 'c-card__check') {
-        const target = e.target;
-        if (target.classList.contains('-checked')) {
-            target.closest('.c-card').dataset.status = 'todo';
-            target.classList.remove('-checked');
-        } else {
-            target.closest('.c-card').dataset.status = 'done';
-            target.classList.add('-checked');
-        }
-        statusFilter();
+//編集（削除）可能にする
+const enableEdit = () => {
+    const todoItem = document.querySelectorAll('.c-card');
+    todoItem.forEach((item) => {
+        item.classList.add('-edit');
+    });
+    editBtn.classList.add('is-active');
+};
+
+//編集解除
+const releaseEdit = () => {
+    const todoItem = document.querySelectorAll('.c-card');
+    todoItem.forEach((item) => {
+        item.classList.remove('-edit');
+    });
+    editBtn.classList.remove('is-active');
+};
+
+//編集ボタンクリック時の動き
+const editBtn = document.getElementsByClassName('c-edit')[0];
+editBtn.addEventListener('click', (e) => {
+    if (!editBtn.classList.contains('is-active')) {
+        enableEdit();
+    } else {
+        releaseEdit();
     }
 });
 
-//編集ボタンクリック
-const editBtn = document.getElementsByClassName('c-edit')[0];
-editBtn.addEventListener('click', (e) => {
-    const todoItem = document.querySelectorAll('.c-card');
-    todoItem.forEach((item) => {
-        if (e.target.classList.contains('is-active')) {
-            item.classList.remove('-edit');
-            e.target.classList.remove('is-active');
-            addBtn.removeAttribute('disabled');
-            floatAddBtn.removeAttribute('disabled');
-        } else {
-            item.classList.add('-edit');
-            addBtn.setAttribute('disabled', true);
-            floatAddBtn.setAttribute('disabled', true);
-            e.target.classList.add('is-active');
-        }
-    });
-});
+// //削除のダイアログ表示・非表示
+// const dialog = document.getElementById('js-dialog');
+// const dispDialog = (removeItem) => {
+//     dialog.classList.add('is-active');
+//     dialog.dataset = removeItem;
+// };
+// const hideDialog = () => {
+//     dialog.classList.remove('is-active');
+// };
 
-//削除
-// todoList.addEventListener('click', (e) => {
-//     if (e.target.classList[0] === 'c-card__remove') {
-//         const removeBtn = e.target;
-//         const removeItem = removeBtn.parentNode;
-//         removeItem.parentNode.removeChild(removeItem);
-//         statusFilter();
+// //削除・キャンセル
+// dialog.addEventListener('click', (e) => {
+//     const target = e.target.getAttribute('id');
+//     switch (target) {
+//         case 'yes':
+//             hideDialog();
+//             removeItem.parentNode.removeChild(removeItem);
+//             statusFilter();
+//             break;
+//         case 'cancel':
+//             hideDialog();
+//             break;
+//         default:
 //     }
 // });
+
+//チェック・削除・修正各ボタンクリック
+todoList.addEventListener('click', (e) => {
+    const target = e.target.classList[0];
+    switch (target) {
+        case 'c-card__check':
+            if (e.target.classList.contains('-checked')) {
+                e.target.closest('.c-card').dataset.status = 'todo';
+                e.target.classList.remove('-checked');
+            } else {
+                e.target.closest('.c-card').dataset.status = 'done';
+                e.target.classList.add('-checked');
+            }
+            statusFilter();
+            break;
+        case 'c-card__remove':
+            const removeItem = e.target.parentNode;
+            // dispDialog(removeItem);
+            removeItem.parentNode.removeChild(removeItem);
+            statusFilter();
+            break;
+        case 'c-card__reEdit':
+            const task = e.target.parentNode.getElementsByClassName(
+                'c-card__body'
+            )[0];
+            const taskContent = task.textContent;
+            const input = `<input type="text" class="c-card__input" value="${taskContent}">`;
+            task.parentNode.insertAdjacentHTML('beforeend', input);
+            task.parentNode.removeChild(task);
+            e.target.closest('.c-card').classList.add('-input');
+
+            const len = taskContent.length;
+            document.getElementsByClassName('c-card__input')[0].focus();
+            document
+                .getElementsByClassName('c-card__input')[0]
+                .setSelectionRange(len, len); //カーソルを一番後ろにする
+            break;
+        default:
+    }
+});
